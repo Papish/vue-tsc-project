c7d52d9f77ff7f04c61e5a11a5ce3fb827274cbe
Papish Limbu <lpapish97@gmail.com>
2023-01-26 21:06:07 +0545

    wip

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>content</div>
 </template>
+
+<style></style>
